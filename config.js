715d1363b85c056f7266f236c24a3d26dbc7b970
maxytedesco <102389819+maximilianoTedesco@users.noreply.github.com>
2026-07-08 11:36:20 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,33 @@
+const CONFIG = {
+    minutosAntesEntrada: 90,
+
+    carrinhas: [
+        {
+            nome: "Carrinha 1",
+            capacidade: 15
+        },
+        {
+            nome: "Carrinha 2",
+            capacidade: 8
+        },
+        {
+            nome: "Carrinha 3",
+            capacidade: 8
+        },
+        {
+            nome: "Carrinha 4",
+            capacidade: 8
+        },
+        {
+            nome: "Carrinha 5",
+            capacidade: 8
+        }
+    ],
+
+    colunas: {
+        nome: 0,
+        hotel: 2,
+        entrada: 3,
+        recolha: 5
+    }
+};
